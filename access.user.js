beced1888188cefdca31af6c0653260897847c1c
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 13:58:20 +0100

    Create access.user.js

---
+++ access.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos access Toolkit
+// @namespace    https://aternos.org/access
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/access/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
