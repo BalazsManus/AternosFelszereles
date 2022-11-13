05426c861c179596a505f6721d66451b6caf8ad9
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 14:01:20 +0100

    Create options.user.js

---
+++ options.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Aternos Settings Toolkit
+// @namespace    https://aternos.org/options
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/options/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+//fisfos szarok eltávolítása
+$('.sidebar').css('display', 'none');
+$('.ad-label-wrapper').css('display', 'none');
+$('#placement-top-leaderboard').css('display', 'none');
+$('.responsive-leaderboard').css('display', 'none');
