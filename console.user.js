f0faaccc7928d3759d3335885a181c5529dbed78
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 13:59:32 +0100

    Create console.user.js

---
+++ console.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Aternos Console Toolkit
+// @namespace    https://aternos.org/console
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/console/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+//fisfos szarok eltávolítása
+$('.sidebar').css('display', 'none');
+$('.ad-label-wrapper').css('display', 'none');
+$('#placement-top-leaderboard').css('display', 'none');
+$('.responsive-leaderboard').css('display', 'none');
