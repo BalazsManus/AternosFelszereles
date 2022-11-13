100d0eb3bf36c745f4e4a6c6df058234847a9634
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 14:00:27 +0100

    Create players.user.js

---
+++ players.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos players Toolkit
+// @namespace    https://aternos.org/players
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/players/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
