7bf41030f90aa8bf75571bf771a40d333935e768
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 14:02:14 +0100

    Create worlds.user.js

---
+++ worlds.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos worlds Toolkit
+// @namespace    https://aternos.org/worlds
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/worlds/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
