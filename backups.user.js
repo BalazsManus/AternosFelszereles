c4151b26d59b887b65007108c411ba1b34d5b54d
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 13:59:14 +0100

    Create backups.user.js

---
+++ backups.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos backups Toolkit
+// @namespace    https://aternos.org/backups
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/backups/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
