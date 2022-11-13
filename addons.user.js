5ad527a77108a6855e767e12741b1748537a07d7
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 13:58:55 +0100

    Create addons.user.js

---
+++ addons.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos addons Toolkit
+// @namespace    https://aternos.org/addons
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/addons/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
