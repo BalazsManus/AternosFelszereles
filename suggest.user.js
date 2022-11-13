c34c1517f6943ec131f3526aa46593593495c435
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 14:01:58 +0100

    Create suggest.user.js

---
+++ suggest.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos suggest Toolkit
+// @namespace    https://aternos.org/suggest
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/suggest/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
