c27d7bfa9e6a5ac610c85ddb1a0b07cdc4172019
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2022-11-13 14:00:06 +0100

    Create log.user.js

---
+++ log.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Aternos log Toolkit
+// @namespace    https://aternos.org/log
+// @version      1.0
+// @description  Aternos Felszerelés Balazsmanustól
+// @author       BalazsManus
+// @match        https://aternos.org/log/*
+// @grant        none
+// @icon         https://www.spigotmc.org/favicon.ico
+// ==/UserScript==
+// Elhozta neked BalazsManus :D
+
+$('#placement-account-bottom').remove();
+$('#placement-takeover').remove();
+$('.responsive-leaderboard').remove();
+$('.sidebar').css('display', 'none');
